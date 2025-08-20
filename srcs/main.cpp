5fc4c230b8fbe7c862e2e50c40be4d72bcb1bba0
Thomas Robert <throbert@c1r7p5.42lehavre.fr>
2025-08-20 17:22:54 +0200

    Add: Parsing ports and password

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -24,13 +24,32 @@ void setupSignal()
 
 }
 
+bool checkArgs(int port, std::string password)
+{
+	if (port < 1024 || port > 65535)
+	{
+		std::cout << "Port needs to be between 1024 and 65535" << std::endl;
+		return false;
+	}
+	if (password.length() > 18)
+	{
+		std::cout << "Password too long" << std::endl;
+		return false;
+	}
+	return true;
+}
+
+
 int main(int ac, char **av) {
 
 	if (ac != 3) {
 		std::cout << "usage: ./ircserv <port> <password>" << std::endl;
 		return (1);
 	}
+	if (!checkArgs(std::atoi(av[PORT]), av[PASSWORD]))
+		return (1);
 	try {
+		
 		Server serv;
 		setupSignal();
 		serv.initServer(std::atoi(av[PORT]), av[PASSWORD]);
